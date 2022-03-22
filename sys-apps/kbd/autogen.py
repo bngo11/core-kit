814ae63624e9a1dd735269719ff57f36b02fc97d
shadow <shadow.ngo@gmail.com>
2022-03-22 14:01:59 -0500

    Autogenerated tree updates.
    merged:
    
      flora: 523a446b976b43c4ede7fd49048643aaf4812e42
      gentoo-staging: 9292583023a6610905c79a4707db3ac6c63b3a31
      kit-fixups: f0f512618dd10e3d9f8995590c41c630a6e6a890

--- sys-apps/kbd/autogen.py
+++ sys-apps/kbd/autogen.py
@@ -1,25 +1,33 @@
 #!/usr/bin/env python3
 
+import json
 
 async def generate(hub, **pkginfo):
-	user = "legionus"
-	repo = pkginfo["name"]
-	releases = await hub.pkgtools.fetch.get_page(f"https://api.github.com/repos/{user}/{repo}/releases", is_json=True)
+	json_data = await hub.pkgtools.fetch.get_page("https://api.github.com/repos/legionus/kbd/releases", is_json=True)
 	version = None
-	for release in releases:
-		if release["prerelease"] or release["draft"]:
-			continue
-		version = release["tag_name"].lstrip("v")
-		break
-	url = f"https://github.com/{user}/{repo}/archive/refs/tags/v{version}.tar.gz"
-	src_artifact = hub.pkgtools.ebuild.Artifact(url=url, final_name=f"kbd-{version}.tar.gz")
-	ebuild = hub.pkgtools.ebuild.BreezyBuild(
-		**pkginfo,
-		version=version,
-		github_user=user,
-		github_repo=repo,
-		artifacts=[src_artifact],
-	)
-	ebuild.push()
+	url = None
 
-# vim: sw=4 ts=4 noet
+	for item in json_data:
+		try:
+			if item["prerelease"] or item["draft"]:
+				continue
+
+			version = item["tag_name"].lstrip("v")
+			list(map(int, version.split(".")))
+			break
+
+		except (KeyError, IndexError, ValueError):
+			continue
+
+	if version:
+		final_name = f"kbd-{version}.tar.gz"
+		url = f"https://github.com/legionus/kbd/archive/refs/tags/v{version}.tar.gz"
+
+		ebuild = hub.pkgtools.ebuild.BreezyBuild(
+			**pkginfo,
+			version=version,
+			artifacts=[hub.pkgtools.ebuild.Artifact(url=url, final_name=final_name)]
+		)
+		ebuild.push()
+
+# vim: ts=4 sw=4 noet
