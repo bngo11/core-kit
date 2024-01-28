df94582a59ed15dcfb10f26783fdc533b79250be
shadow <shadowngoz@gmail.com>
2024-01-28 16:57:44 -0600

    Autogenerated tree updates.
    merged:
    
      flora: 523a446b976b43c4ede7fd49048643aaf4812e42
      gentoo-staging: 9292583023a6610905c79a4707db3ac6c63b3a31
      gentoo-staging-nov-2021: a15a64f48741a493e49dfee7d4280a3dc10cd21d
      kit-fixups: d855519f63052adcf8e39c86b2723132b3341927

--- app-shells/fzf/autogen.py
+++ app-shells/fzf/autogen.py
@@ -1,8 +1,15 @@
 #!/usr/bin/env python3
 
 import json
+from bs4 import BeautifulSoup
 
 async def generate(hub, **pkginfo):
+	html_data = await hub.pkgtools.fetch.get_page("https://dev.gentoo.org/~sam/distfiles/app-shells/fzf/")
+	soup = BeautifulSoup(html_data, "html.parser")
+	links = soup.find_all("a")
+	links.reverse()
+	deps_ver = links[0].get("href").split("-")[1]
+
 	json_data = await hub.pkgtools.fetch.get_page("https://api.github.com/repos/junegunn/fzf/releases", is_json=True)
 	version = None
 
@@ -13,7 +20,8 @@ async def generate(hub, **pkginfo):
 
 			version = item["tag_name"]
 			list(map(int, version.split(".")))
-			break
+			if version == deps_ver:
+				break
 
 		except (KeyError, IndexError, ValueError):
 			continue
