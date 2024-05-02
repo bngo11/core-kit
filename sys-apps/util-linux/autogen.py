69de2bbcfc54260564c27b7a93170df2b5d71793
shadow <shadowngoz@gmail.com>
2024-05-02 14:57:26 -0500

    Autogenerated tree updates.
    merged:
    
      flora: 523a446b976b43c4ede7fd49048643aaf4812e42
      gentoo-staging: 9292583023a6610905c79a4707db3ac6c63b3a31
      gentoo-staging-nov-2021: a15a64f48741a493e49dfee7d4280a3dc10cd21d
      kit-fixups: d6434c9f255ddec71b2e0587fbd689f2f94dc8d7

--- sys-apps/util-linux/autogen.py
+++ sys-apps/util-linux/autogen.py
@@ -1,29 +1,30 @@
 #!/usr/bin/env python3
 
-from bs4 import BeautifulSoup
+import json
 import re
 
 async def generate(hub, **pkginfo):
-	python_compat="python3+"
-	tags_url = f"https://git.kernel.org/pub/scm/utils/util-linux/util-linux.git/refs/tags/"
-	html_data = await hub.pkgtools.fetch.get_page(tags_url)
-	soup = BeautifulSoup(html_data, "html.parser")
-	best_archive = None
-	for link in soup.find_all("a"):
-		href = link.get("href")
-		fn = href.split("/")[-1]
-		if re.match('util-linux-[0-9.]+\.tar', fn):
-			best_archive = href
+	json_data = await hub.pkgtools.fetch.get_page("https://api.github.com/repos/util-linux/util-linux/tags", is_json=True)
+	version = None
+	BASEVER = re.compile('\d+\.\d+')
+
+	for item in json_data:
+		try:
+			version = item["name"].strip('v')
+			list(map(int, version.split(".")))
 			break
-	version = best_archive.split(".tar")[0].split("-")[-1]
-	shortver = ".".join(version.split(".")[0:2])
-	url = f"https://www.kernel.org/pub/linux/utils/util-linux/v{shortver}/util-linux-{version}.tar.xz"
-	artifact = hub.pkgtools.ebuild.Artifact(url=url)
 
-	ebuild = hub.pkgtools.ebuild.BreezyBuild(
-		**pkginfo, version=version, python_compat=python_compat, artifacts=[artifact],
-	)
-	ebuild.push()
+		except (KeyError, IndexError, ValueError):
+			continue
 
+	if version:
+		url = f"https://www.kernel.org/pub/linux/utils/util-linux/v{BASEVER.match(version).group()}/util-linux-{version}.tar.xz"
+		ebuild = hub.pkgtools.ebuild.BreezyBuild(
+			**pkginfo,
+			version=version,
+			python_compat='python3+',
+			artifacts=[hub.pkgtools.ebuild.Artifact(url=url, final_name=url.rsplit('/', 1)[-1])]
+		)
+		ebuild.push()
 
 # vim: ts=4 sw=4 noet
