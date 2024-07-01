b679c9dbaa84df7e2b99e083e29bb42b9982f0ab
shadow <shadowngoz@gmail.com>
2024-07-01 15:57:29 -0500

    Autogenerated tree updates.
    merged:
    
      flora: 523a446b976b43c4ede7fd49048643aaf4812e42
      gentoo-staging: 9292583023a6610905c79a4707db3ac6c63b3a31
      gentoo-staging-nov-2021: a15a64f48741a493e49dfee7d4280a3dc10cd21d
      kit-fixups: 2e9b7c93c153ee855c187ff9073814ab7ab34c8d

--- app-emulation/incus/autogen.py
+++ app-emulation/incus/autogen.py
@@ -3,17 +3,25 @@
 import json
 
 async def generate(hub, **pkginfo):
-	json_data = await hub.pkgtools.fetch.get_page("https://api.github.com/repos/lxc/incus/releases", is_json=True)
+	json_data = await hub.pkgtools.fetch.get_page("https://api.github.com/repos/lxc/incus/tags", is_json=True)
 	version = None
 	url = None
 
 	for item in json_data:
 		try:
-			if item["prerelease"] or item["draft"]:
-				continue
-
-			version = item["tag_name"].strip('v')
+			version = item["name"].strip('v')
 			list(map(int, version.split(".")))
+			break
+
+		except (KeyError, IndexError, ValueError):
+			continue
+
+	json_data = await hub.pkgtools.fetch.get_page("https://api.github.com/repos/lxc/incus/releases", is_json=True)
+
+	for item in json_data:
+		try:
+			if item["tag_name"].strip('v') != version:
+				continue
 
 			for asset in item['assets']:
 				asset_name = asset["name"]
