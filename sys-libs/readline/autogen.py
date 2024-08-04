7922d15543d7100fc5258e6446e6bd3f31a3e6d4
shadow <shadowngoz@gmail.com>
2024-08-03 20:59:39 -0500

    Autogenerated tree updates.
    merged:
    
      flora: 523a446b976b43c4ede7fd49048643aaf4812e42
      gentoo-staging: 9292583023a6610905c79a4707db3ac6c63b3a31
      gentoo-staging-nov-2021: a15a64f48741a493e49dfee7d4280a3dc10cd21d
      kit-fixups: 75d4be9df1be27be3ccdd8cf2b2042eaeaee8aef

--- sys-libs/readline/autogen.py
+++ sys-libs/readline/autogen.py
@@ -1,60 +1,55 @@
 #!/usr/bin/env python3
 
 from bs4 import BeautifulSoup
-from metatools.version import generic
-import os
-import re
-
-base_url = "https://ftp.gnu.org/gnu/"
-base_regex = r'(\d+(?:\.\d+)+)'
-mask_above = generic.parse("8.2")
 
 async def generate(hub, **pkginfo):
-	name = pkginfo['name']
-	regex = name + '-' + base_regex
-	stable_regex = base_regex + '.tar.gz'
-	package_url = base_url + name
+	base_url = f"https://ftp.gnu.org/gnu/{pkginfo['name']}"
+	html_data = await hub.pkgtools.fetch.get_page(base_url)
+	soup = BeautifulSoup(html_data, "html.parser")
+	links = soup.find_all("a", href = True)
+	links.reverse()
+	version = None
+	base_version = None
+	soname = None
+	prestr = ['alpha', 'beta', 'rc', 'pr']
+	release = []
 
-	tarballs = [t for t in await fetch_soup(hub, package_url, '.tar.') if  not '-doc' in t.contents[0]]
-	versions = [(generic.parse(re.findall(regex, a.contents[0])[0]), a.get('href')) for a in tarballs if re.findall(stable_regex, a.contents[0])]
-	latest = max([v for v in versions if not v[0].is_prerelease])
-	#latest = max([v for v in versions if v[0] < mask_above and not v[0].is_prerelease])
+	for link in links:
+		try:
+			contents = link.contents[0]
+			name, ver = contents.split('-', 1)
+			if ver.startswith('doc-'):
+				continue
+			if any(y in ver for y in prestr):
+				continue
 
-	tarball_artifact = [hub.pkgtools.ebuild.Artifact(url=f"{package_url}/{latest[1]}")]
+			version, extra = ver.split(".tar.gz")
+			if extra:
+				continue
+			versplit = version.split('.')
+			list(map(int, versplit))
+			if versplit > release:
+				release = versplit
+				continue
+			version = ".".join(release)
+			base_version = ".".join(release[:2])
+			soname = release[0]
+			break
+		except ValueError:
+			continue
 
-	# get patches for versions that don't have a micro (e.g. 8.1 or 8.2, but not 8.1.2 or 8.2.1)
-	patch_level, patch_artifacts = await fetch_patches(hub, package_url, name, latest[0])
-	version = f"{latest[0].public}"
-	if patch_level:
-		version += f"_p{patch_level}"
+	if version:
+		final_name = f"readline-{version}.tar.gz"
+		url = f"{base_url}/{final_name}"
+		ebuild = hub.pkgtools.ebuild.BreezyBuild(
+			**pkginfo,
+			version=version,
+			base_version=base_version,
+			soname=soname,
+			artifacts=[hub.pkgtools.ebuild.Artifact(url=url, final_name=final_name)],
+		)
 
-	ebuild = hub.pkgtools.ebuild.BreezyBuild(
-		**pkginfo,
-		soname=latest[0].major,
-		base_version=latest[0].public,
-		version=version,
-		artifacts=tarball_artifact + patch_artifacts,
-		patches=[p.final_name for p in patch_artifacts],
-	)
-	ebuild.push()
+		ebuild.push()
 
-async def fetch_soup(hub, url, name):
-	html = await hub.pkgtools.fetch.get_page(url)
-	soup = BeautifulSoup(html, 'html.parser').find_all('a', href=True)
-
-	return [a for a in soup if name in a.contents[0] and not a.contents[0].endswith('.sig')]
-
-
-async def fetch_patches(hub, package_url, name, version):
-	url = f"{package_url}/{name}-{version}-patches/"
-	name = f"{name}{version.public.replace('.','')}"
-
-	patches = await fetch_soup(hub, url, name)
-	patch_artifacts = [hub.pkgtools.ebuild.Artifact(url=url + p.get('href')) for p in patches]
-	try:
-		plevel = max([generic.parse(p.contents[0].split('-')[1]) for p in patches]).public
-		return plevel, patch_artifacts
-	except ValueError:
-		return 0, []
 
 # vim: ts=4 sw=4 noet
--- sys-process/procs/procs-0.14.6.ebuild
+++ sys-process/procs/procs-0.14.6.ebuild
@@ -6,8 +6,8 @@ inherit cargo
 
 DESCRIPTION="A modern replacement for ps written in Rust"
 HOMEPAGE="https://github.com/dalance/procs"
-SRC_URI="https://github.com/dalance/procs/tarball/4f1f6ab87c8f10e7369c1b730d946f333b0fb64c -> procs-0.14.5-4f1f6ab.tar.gz
-https://direct.funtoo.org/69/45/dc/6945dcab390228f1902a8a7b1aef6d6b855a2b0ee17f9b8b876e620ba970b52fe0602baf0ba4ec9033d16900861e85f7837fb176659943db8021d2d2e47207e9 -> procs-0.14.5-funtoo-crates-bundle-092e92061c85073f98abfeb1952e66e19db623d838a42c5f9b62a15c654e2e2b9268b4be4da5464dfef6b29fe972c7403d2498634f51a0272a5a2d0253dee292.tar.gz"
+SRC_URI="https://github.com/dalance/procs/tarball/c6d6ca4afd0a1358e77da966931fc2e50213a2cd -> procs-0.14.6-c6d6ca4.tar.gz
+https://direct.funtoo.org/a5/e1/68/a5e168072bd9ef549e8afe3124be80eab780aa50a8fa37d6bec839d423cd7f3d72205974624b07e338a6279157b51ec8d6be08ab1fbe90c0c7c1154b0177b062 -> procs-0.14.6-funtoo-crates-bundle-025ce06dfb896ee482744477f1ed3445e32f3b2829a89c85561dec6964881b7a2e63798e6261fb0f0c3158270654effac6bbebb5d0a763783e89833669e70723.tar.gz"
 
 LICENSE="Apache-2.0 BSD BSD-2 CC0-1.0 MIT ZLIB"
 SLOT="0"
