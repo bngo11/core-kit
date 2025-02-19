170425ef2869c3c0b685181d0f148829fbeb2b05
shadow <shadowngoz@gmail.com>
2025-02-18 20:02:15 -0600

    Autogenerated tree updates.
    merged:
    
      flora: 523a446b976b43c4ede7fd49048643aaf4812e42
      gentoo-staging: 9292583023a6610905c79a4707db3ac6c63b3a31
      gentoo-staging-nov-2021: a15a64f48741a493e49dfee7d4280a3dc10cd21d
      kit-fixups: 3bc32f509499e016554490c347cc871728819fb6

---
+++ sys-apps/util-linux/autogen.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+import json
+
+async def generate(hub, **pkginfo):
+	github_user = github_repo = pkginfo.get("name")
+	json_data = await hub.pkgtools.fetch.get_page(f"https://api.github.com/repos/{github_user}/{github_repo}/tags", is_json=True)
+	version = None
+	url = None
+
+	for item in json_data:
+		try:
+			version = item["name"].lstrip("v")
+			list(map(int, version.split(".")))
+			final_name = f"{github_repo}-{version}.tar.xz"
+			url = f"https://mirrors.edge.kernel.org/pub/linux/utils/{github_repo}/v{version.rsplit('.', 1)[0]}/{final_name}"
+			break
+
+		except (KeyError, IndexError, ValueError):
+			continue
+
+	if version and url:
+		ebuild = hub.pkgtools.ebuild.BreezyBuild(
+			**pkginfo,
+			version=version,
+			artifacts=[hub.pkgtools.ebuild.Artifact(url=url, final_name=final_name)]
+		)
+		ebuild.push()
+
+# vim: ts=4 sw=4 noet
